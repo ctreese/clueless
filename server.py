5e5119ed4c1879496901b1e7064ed0caeb75781b
Brandon Hogge <bhogge1@gmail.com>
2019-10-21 22:02:26 -0400

    set CORS policy in server

--- server.py
+++ server.py
@@ -123,10 +123,35 @@ class initResource(object):
     def on_post(self, req, resp):
         resp.set_header('Powered-By', 'Falcon')
         resp.status = falcon.HTTP_201
+        
+class CORSComponent(object):
+	def process_response(self, req, resp, resource, req_succeeded):
+		resp.set_header('Access-Control-Allow-Origin', '*')
+
+		if (req_succeeded
+			and req.method == 'OPTIONS'
+			and req.get_header('Access-Control-Request-Method')
+		):
+			# NOTE(kgriffs): This is a CORS preflight request. Patch the
+			#   response accordingly.
+
+			allow = resp.get_header('Allow')
+			resp.delete_header('Allow')
+
+			allow_headers = req.get_header(
+				'Access-Control-Request-Headers',
+				default='*'
+			)
+
+			resp.set_headers((
+				('Access-Control-Allow-Methods', allow),
+				('Access-Control-Allow-Headers', allow_headers),
+				('Access-Control-Max-Age', '86400'),  # 24 hours
+			))
 
 
 # Configure your WSGI server to load "things.app" (app is a WSGI callable)
-app = falcon.API()
+app = falcon.API(middleware=[CORSComponent()])
 
 gs = Gamestate()
 
